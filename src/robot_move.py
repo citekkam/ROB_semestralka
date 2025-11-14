af07f792598b455e1fec1b1418ac54555abeb282
XuanDinhNguyenhck <dinhhck@gmail.com>
2025-11-14 08:47:16 +0100

    todo fix current

--- src/robot_move.py
+++ src/robot_move.py
@@ -337,7 +337,7 @@ class RobotMove:
         return True
 
 
-    def ik_sol_check(self, current_q: np.ndarray, target_T: np.ndarray, seq : list, coli_seq : list, check_angle = True) -> [bool, int]:
+    def ik_sol_check(self, current_q: np.ndarray, target_T: np.ndarray, seq : list, coli_seq : list, check_angle = True, prev_q) -> [bool, int]:
         """check if there is an ik solution within range pi/2 from current_q,
             check if it is within robot limits and
             check the collisions
@@ -350,10 +350,12 @@ class RobotMove:
             # todo collision check
             if check_angle:
                 if self.dif_angle_check(current_q, q, np.pi* 2/4) and self.robot.in_limits(q) and not self.collision.in_collision(q, coli_seq):
+                    prev_q = q
                     return True, idx
             else:
                 print("checking start")
                 if self.robot.in_limits(q) and not self.collision.in_collision(q, coli_seq):
+                    prev_q = q
                     print("Robot not in limits or collision")
                     return True, idx
         return False, None
@@ -362,10 +364,11 @@ class RobotMove:
         # print("target T : ", seq[0] * CRC_OFF.inverse())
         # print("seq :", *seq, sep="\n")
         # print(seq)
+        prev_q = current_q
         for T in seq:
             target_T = SE3(translation = [0,0,-0.02]) * T * CRC_OFF.inverse()
             # is_valid, idx = self.ik_sol_check(current_q, target_T.homogeneous())
-            is_valid, idx = self.ik_sol_check(current_q, target_T.homogeneous(), seq, coli_seq, check_angle)
+            is_valid, idx = self.ik_sol_check(current_q, target_T.homogeneous(), seq, coli_seq, check_angle, prev_q)
             if not is_valid:
                 print("Cant get to :", target_T)
                 return False
